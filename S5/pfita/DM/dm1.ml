7f59b107ee3f01dc6d2eb891d1f01e6a380ae61a
arxaqapi <arxaqapi@protonmail.com>
2020-10-31 00:45:05 +0100

    bckp 31/10/2020

--- S5/pfita/DM/dm1.ml
+++ S5/pfita/DM/dm1.ml
@@ -11,8 +11,6 @@ let score (_, _, _, list_votes) =
     | _ :: l -> 1 + length l
   in length list_votes;;
 
-  
-
 (* Q 3.1 *)
 let rec add_once e l = match l with 
   | [] -> e :: []
@@ -54,15 +52,85 @@ let rec vote_comment user_id comment_id (com_base : comments) : comments = match
       else
       if is_the_comment
       then (n, (c_id, u_id, txt, (add_once user_id u_id_l)) :: tl)
-      else (fst com_base, (c_id, u_id, txt, u_id_l) :: (snd (vote_comment user_id comment_id (n, tl))))
+      else (n, (c_id, u_id, txt, u_id_l) :: (snd (vote_comment user_id comment_id (n, tl))))
+
+(* Q 4.3 *)
+let rec get_comment comment_id com_base = match (com_base : comments) with
+  | (n, []) -> failwith "not found"
+  | (n, (c_id, u_id, txt, u_id_l) :: tl) -> 
+      if comment_id = c_id 
+      then txt 
+      else get_comment comment_id (n, tl)
+
+(* Q 4.4 *)
+let rec get_comments author_id com_base = match (com_base : comments) with
+  | (n, []) -> []
+  | (n, (c_id, u_id, txt, u_id_l) :: tl) -> 
+      if author_id = u_id 
+      then txt :: get_comments author_id (n, tl)
+      else get_comments author_id (n, tl)
 
 
-(* Q 4.3 
-let get_comment comment_id com_base = 
-  let rec gc cl = match cl with
-    | [] -> failwith "not found"
-    | hd :: tl -> if comment_id = (snd hd) then (thr hd) else gc tl
-  in
-  gc (snd com_base)
+(* fnc that checks if voted for a comment in a com_base *)
+let rec has_voted user_id u_id_l = match (u_id_l : user_id list) with
+  | [] -> false
+  | hd :: tl -> hd = user_id || has_voted user_id tl
+
+
+(* Q 4.5 *)     
+let rec get_voted_comments user_id com_base = match (com_base : comments) with
+  | (n, []) -> []
+  | (n, (c_id, u_id, txt, u_id_l) :: tl) ->
+      if has_voted user_id u_id_l
+      then txt :: get_voted_comments user_id (n, tl)
+      else get_voted_comments user_id (n, tl)
+
+
+(* Q 4.6 *)
+(* when we remove an element, we do not update (fst comments)
+  because it remembers the amount of comments created from the beginning
 *)
+let rec remove_comment comment_id (comments : comments) : comments = match comments with
+  | (n, []) -> failwith "not found"
+  | (n, (c_id, u_id, txt, u_id_l) :: tl) ->
+      if comment_id = c_id 
+      then (n, tl)
+      else (n, (c_id, u_id, txt, u_id_l) :: (snd (remove_comment comment_id (n, tl))))
 
+
+(* Q 4.7 *)
+let rec remove_comments user_id (comments : comments) : comments = match comments with
+  | (n, []) -> (n, [])
+  | (n, (c_id, u_id, txt, u_id_l) :: tl) -> 
+      if user_id = u_id 
+      then (n, snd (remove_comments user_id (n, tl)))
+      else (n, (c_id, u_id, txt, u_id_l) :: (snd (remove_comments user_id (n, tl))))
+
+
+(* Q 4.8 *)
+let rec update_comment user_id comment_id f comments : comments = match comments with
+  | (n, []) -> failwith "not found"
+  | (n, (c_id, u_id, txt, u_id_l) :: tl) -> 
+      if (comment_id = c_id) && (user_id <> u_id)
+      then failwith "author"
+      else if (comment_id = c_id) && (user_id = u_id)
+      then (n, (c_id, u_id, f txt, u_id_l) :: tl)
+      else (n, (c_id, u_id, txt, u_id_l) :: (snd (update_comment user_id comment_id f (n, tl))))
+
+
+(* ma of 2 ints *)
+let max a b = 
+  if a >= b 
+  then a
+  else if a < b
+  then b
+
+(* Q 4.9 *)
+let rec top_three comments = 
+  let rec tr cl a1 a2 a3 = match cl with
+    | (c_id, u_id, txt, u_id_l) -> true
+  in
+  tr snd comments 0 0 0
+
+
+(* force int in comments fields:-user_id, bcs 'a accepts every type *)
