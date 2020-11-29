43e6583d4faeeed311f235f861f074e8219b6b4f
arxaqapi <arxaqapi@protonmail.com>
2020-11-29 23:34:15 +0100

    cleaning syst dir

--- S5/systeme/tp/tp4_5/afficher.c
+++ S5/systeme/tp/tp4_5/afficher.c
@@ -6,3 +6,12 @@ void afficher (void) {
   printf("Processus (fils) de pid %d : Je boucle...\n", getpid());
   sleep(1);
 }
+
+int main(void)
+{
+  while (1)
+  {
+    afficher();
+  }
+  return 0;
+}
